a9a0fd9aeb6bc6e77c2eb0b57fcb53b4adfd3bd9
d.o.kononov <d.o.kononov@gmail.com>
2025-09-19 13:06:34 +0300

    CoreImage + filters

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,27 +6,54 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     
     var body: some View {
-            Button("Hello, world") {
-                showingConfirmation.toggle()
-            }
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .confirmationDialog("Change background color", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) {}
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear {
+            loadImage()
+        }
+    }
+    
+    private func loadImage()  {
+        let inputImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        
+        currentFilter.inputImage = beginImage
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
                 
-            } message: {
-                Text("Select new color")
-            }
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        
+        image = Image(uiImage: uiImage)
+        
     }
 }
 
