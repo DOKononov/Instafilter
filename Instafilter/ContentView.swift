69575a3723b44b603575cec8199ff9a23490cc34
d.o.kononov <d.o.kononov@gmail.com>
2025-09-23 10:06:54 +0300

    requestReview

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,19 +6,15 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
+    @Environment(\.requestReview) private var requestReview
 
     var body: some View {
-        let example = Image(.example)
-        ShareLink(item: example, preview: SharePreview("Image to share", image: example)) {
-            Label("Share me...", systemImage: "swift")
+        Button("Request review") {
+            requestReview()
         }
-//        ShareLink(
-//            item: URL( string: "https://www.youtube.com/watch?v=dQw4w9WgXcQ")!,
-//            subject: Text("Best free course of SwiftUI"),
-//            message: Text("Without registration and sms")
-//        )
     }
 }
 
