8a5322814744b429088e64402e334657db70a4e6
d.o.kononov <d.o.kononov@gmail.com>
2025-09-23 12:46:15 +0300

    add photopicker

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,27 +6,36 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    
+    @State private var selectedItem: PhotosPickerItem?
+    
     
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
                 
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView(
-                        "No picture",
-                        systemImage: "photo.badge.plus",
-                        description: Text("Tap to import a photo")
-                    )
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView(
+                            "No picture",
+                            systemImage: "photo.badge.plus",
+                            description: Text("Tap to import a photo")
+                        )
+                    }
                 }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+
                 
                 Spacer()
                 
@@ -49,6 +58,14 @@ struct ContentView: View {
     
     private func changeFilter() {
         
+    }
+    
+    private func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+        }
     }
 }
 
